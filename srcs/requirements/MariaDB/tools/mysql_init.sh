2f400e95e41cfc2aca9a985a91efe6997207dd70
Julie Blaye <jublaye@hotmail.fr>
2024-11-07 15:45:00 +0100

    [MOD] one step further

--- srcs/requirements/MariaDB/tools/mysql_init.sh
+++ srcs/requirements/MariaDB/tools/mysql_init.sh
@@ -1,7 +1,42 @@
-echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" > /etc/mysql/init.sql
-echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" >> /etc/mysql/init.sql
-echo "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> /etc/mysql/init.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" >> /etc/mysql/init.sql
-echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
+#!/bin/sh
 
+# echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" > /etc/mysql/init.sql
+# echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> /etc/mysql/init.sql
+# echo "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" >> /etc/mysql/init.sql
+# echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
+
+# mysqld_safe
+
+# systemctl status mariadb
+
+echo "INCEPTION: STARTING MARIADB"
+service mariadb start &&\
+USER_EXISTS=$(mariadb -u "$SQL_ROOT_USER" -p"$SQL_ROOT_PASSWORD" -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$SQL_USER');")
+
+
+if [ "$USER_EXISTS" -eq 1 ]; then
+    echo "INCEPTION: User '$SQL_USER' already exists in the database."
+else
+	echo "INCEPTION:  We need to configure the database"
+
+	echo "	creating database"
+	mariadb -u${SQL_ROOT_USER} -p${SQL_ROOT_PASSWORD}  -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" &&\
+	echo "	database created"
+	echo "	creating user ${SQL_USER}"
+	mariadb -u${SQL_ROOT_USER} -p${SQL_ROOT_PASSWORD}  -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';" &&\
+	echo "	user created"
+	echo "	setting privileges for user ${SQL_USER}"
+	mariadb -u${SQL_ROOT_USER} -p${SQL_ROOT_PASSWORD}  -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';" &&\
+	echo "	privileges done for user ${SQL_USER}"
+	echo "	changing admin credentials"
+	mariadb -u${SQL_ROOT_USER} -p${SQL_ROOT_PASSWORD}  -e "ALTER USER '${SQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" &&\
+	echo "	admin credentials changed"
+	echo "	flushing privileges to update all caches"
+	mariadb -u${SQL_ROOT_USER} -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;" && \
+	echo "	caches updated"
+fi
+echo
+echo "INCEPTION: SHUTTING DOWN MARIADB TO RESTART IN SAFE MODE "
+mysqladmin -u${SQL_ROOT_USER} -p${SQL_ROOT_PASSWORD} shutdown &&\
 mysqld_safe
--- srcs/requirements/WordPress/tools/wp_init.sh
+++ srcs/requirements/WordPress/tools/wp_init.sh
@@ -1,24 +1,22 @@
-cd /var/www/html
+# rm -rf /var/www/html/*
 
-if [ -f ./wp-config.php ]
-then
-    echo "Worpress already configured"
-else
-    wp core download --allow-root
-    wp config create --allow-root \
-                    --dbhost=$SQL_HOSTNAME \
-                    --dbname=$SQL_DATABASE \
-                    --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD
-    wp core install --allow-root \
-                    --url=https://$DOMAIN_NAME \
-                    --title=$WP_TITLE \
-                    --admin_user=$WP_ADMIN_USER \
-                    --admin_password=$WP_ADMIN_PASSWORD \
-                    --admin_email=$WP_ADMIN_EMAIL \
-                    --skip-email
-    echo "Wordpress configured"
-fi
+sleep 10
+
+cd /var/www/html
+wp core download --allow-root --path=/var/www/html
+wp config create --allow-root --path=/var/www/html \
+	--dbhost="mariadb" \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD
+wp core install --allow-root --path=/var/www/html \
+	--url=https://$DOMAIN_NAME \
+	--title=$WP_TITLE \
+	--admin_user=$WP_ADMIN_USER \
+	--admin_password=$WP_ADMIN_PASS \
+	--admin_email=$WP_ADMIN_EMAIL \
+	--skip-email
+echo "Wordpress configured"
 
 echo "Starting fpm"
 php-fpm7.4 -F
